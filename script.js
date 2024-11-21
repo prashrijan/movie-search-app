b96677878caf55e9c728e79982ec5084ae4f0f57
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-21 13:41:43 +1100

    show top 20 popular movies

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=466ef338446928731e86da2978827c56&query=";
 
+const search = document.getElementById("search");
+const moviesContainer = document.getElementById("movies-container");
+const spinner = document.getElementById("spinner");
+
 let movies = [];
 
 const getMovies = async () => {
@@ -11,12 +15,37 @@ const getMovies = async () => {
   const data = await res.json();
 
   movies = data.results;
-  console.log(data.results);
-  return movies;
+
+  displayMovie(movies);
+  spinner.classList.add("d-none");
 };
 
-const displayMovie = (lists) => {\
-    lists.fo
+const displayMovie = (lists) => {
+  moviesContainer.innerHTML = "";
+  lists.forEach((item) => {
+    console.log(item);
+    const col = document.createElement("col");
+    col.classList.add("col");
+
+    col.innerHTML = `
+      <div class="card" style="width: 18rem">
+                <img
+                  src="${IMGPATH}${item.poster_path}"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">${item.original_title}</h5>
+                  <p class="card-text text-truncate">
+                    ${item.overview}
+                  </p>
+                  <a href="#" class="btn btn-primary">Add to favorites</a>
+                </div>
+              </div>
+    `;
+
+    moviesContainer.appendChild(col);
+  });
 };
 
-window.addEventListener("load", getMovies);
+getMovies();
