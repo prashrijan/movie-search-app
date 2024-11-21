d2543e97c485943f1d97e04bf94ca2198beca8ed
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-21 23:42:14 +1100

    able to add favorites

--- script.js
+++ script.js
@@ -8,73 +8,126 @@ const search = document.getElementById("search");
 const moviesContainer = document.getElementById("movies-container");
 const spinner = document.getElementById("spinner");
 const inputContainer = document.getElementById("input-container");
+const favBtn = document.getElementById("fav");
+const favoritesContainer = document.getElementById("favorites-container");
+const main = document.getElementById("main");
+const favs = document.getElementById("favs");
 
 let movies = [];
+let favoriteMovies = JSON.parse(localStorage.getItem("favorites")) || [];
 
+// Show Favorites Container
+favBtn.addEventListener("click", () => {
+  main.classList.add("d-none");
+  favs.classList.remove("d-none");
+  displayFavorites();
+});
+
+// Add to Favorites
+const addToFavorites = (movie) => {
+  if (!favoriteMovies.find((fav) => fav.id === movie.id)) {
+    favoriteMovies.push(movie);
+    localStorage.setItem("favorites", JSON.stringify(favoriteMovies));
+    displayFavorites();
+  }
+};
+
+// Display Favorites
+const displayFavorites = () => {
+  favoritesContainer.innerHTML = "";
+  favoriteMovies.forEach((movie) => {
+    const col = document.createElement("div");
+    col.classList.add("col");
+
+    col.innerHTML = `
+      <div class="card" style="width: 18rem">
+        <img
+          src="${IMGPATH}${movie.poster_path}"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">${movie.original_title}</h5>
+          <p class="card-text text-truncate">
+            ${movie.overview}
+          </p>
+        </div>
+      </div>
+    `;
+
+    favoritesContainer.appendChild(col);
+  });
+};
+
+// Fetch and Display Movies
 const getMovies = async () => {
   const res = await fetch(APIURL);
   const data = await res.json();
 
   movies = data.results;
-
   displayMovie(movies);
   spinner.classList.add("d-none");
 };
 
+// Display Movies
 const displayMovie = (lists) => {
   moviesContainer.innerHTML = "";
-  lists.forEach((item) => {
-    const col = document.createElement("col");
+  lists.forEach((movie) => {
+    const col = document.createElement("div");
     col.classList.add("col");
 
     col.innerHTML = `
       <div class="card" style="width: 18rem">
-                <img
-                  src="${IMGPATH}${item.poster_path}"
-                  class="card-img-top"
-                  alt="..."
-                />
-                <div class="card-body">
-                  <h5 class="card-title">${item.original_title}</h5>
-                  <p class="card-text text-truncate">
-                    ${item.overview}
-                  </p>
-                  <a href="#" class="btn btn-primary">Add to favorites</a>
-                </div>
-              </div>
+        <img
+          src="${IMGPATH}${movie.poster_path}"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">${movie.original_title}</h5>
+          <p class="card-text text-truncate">
+            ${movie.overview}
+          </p>
+          <button class="btn btn-primary" data-id="${movie.id}">Add to Favorites</button>
+        </div>
+      </div>
     `;
 
     moviesContainer.appendChild(col);
+
+    // Add Event Listener for Adding to Favorites
+    col
+      .querySelector("button")
+      .addEventListener("click", () => addToFavorites(movie));
   });
 };
 
-getMovies();
-
+// Search Movie
 const searchMovie = async (query) => {
-  const endPoint = SEARCHAPI + query;
-  const res = await fetch(endPoint);
+  const res = await fetch(SEARCHAPI + query);
   const data = await res.json();
-
-  movies = data.results;
-  console.log(movies);
+  return data.results;
 };
 
-inputContainer.addEventListener("input", (e) => {
+// Handle Input Search
+inputContainer.addEventListener("input", async (e) => {
   const value = e.target.value.toLowerCase();
 
-  if (value.length <= 0) {
+  if (!value) {
     displayMovie(movies);
     return;
   }
 
-  searchMovie(value);
-  const filteredMovies = movies.filter((movie) => {
-    return (
+  const searchResults = await searchMovie(value);
+  const filteredMovies = searchResults.filter(
+    (movie) =>
       movie.original_title.toLowerCase().includes(value) ||
       movie.overview.toLowerCase().includes(value)
-    );
-  });
+  );
 
-  moviesContainer.innerHTML = "";
   displayMovie(filteredMovies);
 });
+
+// Initialize App
+getMovies();
+displayFavorites();
